fe2413ea1973d43362cba70a0b0cea9266f17ed5
renzito0637d <renzito0637d@gmail.com>
2025-04-29 18:50:42 -0500

    Creacion de paquetes de spring

---
+++ ucvBackend/src/test/java/com/ucv/UcvBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcvBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
