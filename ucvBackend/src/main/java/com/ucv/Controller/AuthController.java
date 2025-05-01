d70c0a333d201c8e3476d50e7aafa8fea0ed177d
renzito0637d <renzito0637d@gmail.com>
2025-05-01 14:59:21 -0500

    Creando estrucutra de AuthController

--- ucvBackend/src/main/java/com/ucv/Controller/AuthController.java
+++ ucvBackend/src/main/java/com/ucv/Controller/AuthController.java
@@ -1,5 +1,40 @@
 package com.ucv.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ucv.Controller.Model.AuthResponse;
+import com.ucv.Controller.Model.AuthenticationRequest;
+import com.ucv.Controller.Model.RegisterRequest;
+import com.ucv.Services.AuthService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/ucv")
+@RequiredArgsConstructor
 public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthResponse>  authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+
+    @PostMapping("/publictest")
+    public String publicTest() {
+        return "Public Test";
+    }
     
 }
