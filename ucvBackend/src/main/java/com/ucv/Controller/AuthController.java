0e29eed2322e92f01ec73d6396c2b0839a5907eb
renzito0637d <renzito0637d@gmail.com>
2025-05-01 19:45:39 -0500

    Actualizacion Puerbas

--- ucvBackend/src/main/java/com/ucv/Controller/AuthController.java
+++ ucvBackend/src/main/java/com/ucv/Controller/AuthController.java
@@ -2,6 +2,7 @@ package com.ucv.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +33,7 @@ public class AuthController {
         return ResponseEntity.ok(authService.authenticate(request));
     }
 
-    @PostMapping("/publictest")
+    @GetMapping("/publictest")
     public String publicTest() {
         return "Public Test";
     }
