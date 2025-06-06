83edefb45bbd166f04e57a3636decd69ec80abff
Neichi <hotdlolxp12@gmail.com>
2025-06-05 22:52:21 -0500

    Update AssignStaff.jsx

--- ucvfrontend/src/pages/AssignStaff.jsx
+++ ucvfrontend/src/pages/AssignStaff.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import AssignStaffForm from '../components/AssignStaff/AssignStaffForm/AssignStaffForm';
+import AssignStaffTable from '../components/AssignStaff/AssignStaffTable/AssignStaffTable';
 
 const AssignStaff = () => {
-  return <h2>Asignación de Personal</h2>;
+  return (
+    <>
+      <AssignStaffForm />
+      <AssignStaffTable />
+    </>
+  );
 };
 
 export default AssignStaff;
