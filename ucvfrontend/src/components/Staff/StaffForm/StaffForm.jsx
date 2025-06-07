b1d02db8ad223bf8c7c7a04ad0d3018bbf337349
renzito0637d <renzito0637d@gmail.com>
2025-06-07 01:29:05 -0500

    Implement staff registration form with state management and error handling

--- ucvfrontend/src/components/Staff/StaffForm/StaffForm.jsx
+++ ucvfrontend/src/components/Staff/StaffForm/StaffForm.jsx
@@ -1,7 +1,51 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './StaffForm.css';
+import { useState } from 'react';
+import axios from 'axios';
 
-function StaffForm() {
+const StaffForm = () => {
+
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setlLstname] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [nickname, setNickname] = useState("");
+    const [password, setPassword] = useState("");
+    const [cargo, setCargo] = useState("");
+
+    const handleSave = async (e) => {
+        e.preventDefault();
+        const StaffData = {
+            firstname: firstname,
+            lastname: lastname,
+            email: email,
+            phone: phone,
+            nickname: nickname,
+            password: password,
+            cargo: cargo
+        }
+        try {
+            const reponseStaff = await axios.post("http://localhost:8080/api/ucv/register",
+                StaffData, {
+                // Configuración de la solicitud
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+        } catch (error) {
+            // Manejo de errores
+            if (error.reponseStaff) {
+                // El servidor respondió con un código de estado fuera del rango 2xx
+                alert("Error: Credenciales incorrectas");
+            } else if (error.request) {
+                // La solicitud fue hecha pero no hubo respuesta
+                console.error("No se recibió respuesta del servidor:", error.request);
+            } else {
+                // Algo más causó el error
+                console.error("Error al configurar la solicitud:", error.message);
+            }
+        }
+    };
     return (
         <>
             <fieldset className="p-3 bg-light rounded border">
@@ -11,27 +55,37 @@ function StaffForm() {
                     <div className="col-md-3 d-grid gap-2 mb-2">
                         <div>
                             <label className="fw-medium">Nombre</label>
-                            <input type="text" className="form-control" placeholder="Ingrese el nombre del personal" />
+                            <input type="text" className="form-control" placeholder="Ingrese el nombre del personal" 
+                            value={firstname} onChange={(e) => setFirstname(e.target.value)}
+                            />
                         </div>
                         <div>
                             <label className="fw-medium">Apellido</label>
-                            <input type="text" className="form-control" placeholder="Ingrese el apellido del personal" />
+                            <input type="text" className="form-control" placeholder="Ingrese el apellido del personal" 
+                            value={lastname} onChange={(e) => setlLstname(e.target.value)}
+                            />
                         </div>
                     </div>
                     <div className="col-md-3 d-grid gap-2 mb-2">
                         <div>
                             <label className="fw-medium">Correo</label>
-                            <input type="text" className="form-control" placeholder="Ingrese el correo del personal" />
+                            <input type="text" className="form-control" placeholder="Ingrese el correo del personal" 
+                            value={email} onChange={(e) => setEmail(e.target.value)}
+                            />
                         </div>
                         <div>
                             <label className="fw-medium">Telefono móvil</label>
-                            <input type="text" className="form-control" placeholder="Ingrese el telefóno del personal" />
+                            <input type="text" className="form-control" placeholder="Ingrese el telefóno del personal" 
+                            value ={phone} onChange={(e) => setPhone(e.target.value)}
+                            />
                         </div>
                     </div>
                     <div className="col-md-3 d-grid gap-2 mb-2">
                         <div>
                             <label className="fw-medium">Contraseña</label>
-                            <input type="text" className="form-control" placeholder="Ingrese la contraseña del personal" />
+                            <input type="text" className="form-control" placeholder="Ingrese la contraseña del personal" 
+                            value={password} onChange={(e) => setPassword(e.target.value)}
+                            />
                         </div>
                         <div>
                             <label className="fw-medium">Cargo</label>
