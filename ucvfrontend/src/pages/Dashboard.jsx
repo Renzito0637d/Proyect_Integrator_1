3f8d551e4f3b05b1380ec8513272d28142354a0a
Neichi <hotdlolxp12@gmail.com>
2025-06-07 18:44:45 -0500

    Update Dashboard.jsx

--- ucvfrontend/src/pages/Dashboard.jsx
+++ ucvfrontend/src/pages/Dashboard.jsx
@@ -1,14 +1,57 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  FaTachometerAlt,
+  FaClipboardList,
+  FaTags,
+  FaFileAlt,
+  FaBuilding,
+  FaUsers,
+  FaUserPlus,
+} from 'react-icons/fa';
 import DashboardForm from '../components/Dashboard/DashboardForm/DashboardForm';
 import DashboardTable from '../components/Dashboard/DashboardTable/DashboardTable';
 
-const Dashboard = () => {
+export default function Dashboard() {
   return (
-    <>
-      <DashboardForm />
-      <DashboardTable />
-    </>
-  );
-};
+    <div className="main-layout">
+      {/* ===== SIDEBAR ===== */}
+      <div className="sidebar">
+        <Link to="/dashboard" className="sidebar-item">
+          <FaTachometerAlt />
+          <span>Dashboard</span>
+        </Link>
+        <Link to="/incidencias" className="sidebar-item">
+          <FaClipboardList />
+          <span>Incidencias</span>
+        </Link>
+        <Link to="/categoria" className="sidebar-item">
+          <FaTags />
+          <span>Categoría</span>
+        </Link>
+        <Link to="/informe" className="sidebar-item">
+          <FaFileAlt />
+          <span>Informe</span>
+        </Link>
+        <Link to="/departamento" className="sidebar-item">
+          <FaBuilding />
+          <span>Departamento</span>
+        </Link>
+        <Link to="/personal" className="sidebar-item">
+          <FaUsers />
+          <span>Personal</span>
+        </Link>
+        <Link to="/asignar-personal" className="sidebar-item">
+          <FaUserPlus />
+          <span>Asignar</span>
+        </Link>
+      </div>
 
-export default Dashboard;
+      {/* ===== ÁREA DE CONTENIDO ===== */}
+      <div className="content-area">
+        <DashboardForm />
+        <DashboardTable />
+      </div>
+    </div>
+  );
+}
