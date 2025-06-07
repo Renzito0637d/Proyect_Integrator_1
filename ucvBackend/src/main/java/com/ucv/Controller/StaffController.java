b7dc996ba662a5af7eb10e92d02e03ce99189265
Renzito0637d <renzito0637d@gmail.com>
2025-06-06 19:17:47 -0500

    Add controllers for AssignStaff, Category, Deparment, Incident, and Report; update StaffController formatting

--- ucvBackend/src/main/java/com/ucv/Controller/StaffController.java
+++ ucvBackend/src/main/java/com/ucv/Controller/StaffController.java
@@ -3,15 +3,12 @@ package com.ucv.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ucv.Entity.User;
 import com.ucv.Services.StaffService;
-
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/ucv")
@@ -21,6 +18,6 @@ public class StaffController {
 
     @PostMapping("/staffList")
     public List<User> getStaffList() {
-    return staffService.getAll();
-}
+        return staffService.getAll();
+    }
 }
