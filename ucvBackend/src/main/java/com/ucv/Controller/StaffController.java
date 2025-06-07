3220b8e80003b3b7288f0d71a2f056a7c26d03e5
Renzito0637d <renzito0637d@gmail.com>
2025-06-06 20:00:38 -0500

    Add Excel export functionality for staff data and update dependencies Apache POI and google guava

--- ucvBackend/src/main/java/com/ucv/Controller/StaffController.java
+++ ucvBackend/src/main/java/com/ucv/Controller/StaffController.java
@@ -3,12 +3,21 @@ package com.ucv.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ucv.Docs.StaffExcel;
 import com.ucv.Entity.User;
 import com.ucv.Services.StaffService;
+
+import com.ucv.Docs.StaffExcel;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 
 @RestController
 @RequestMapping("/api/ucv")
@@ -19,5 +28,17 @@ public class StaffController {
     @PostMapping("/staffList")
     public List<User> getStaffList() {
         return staffService.getAll();
+    }
+
+    @PostMapping("/staffExcel")
+    public ResponseEntity<byte[]> downloadStaffExcel() throws Exception {
+        List<User> staffList = staffService.getAll();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        StaffExcel.writeStaffToExcel(staffService.getAll(), new File("ruta/deseada/staff.xlsx"));
+
+        return ResponseEntity.ok()
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=staff.xlsx")
+            .contentType(MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"))
+            .body(out.toByteArray());
     }
 }
