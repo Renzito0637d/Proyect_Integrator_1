7f2a1c4b6d7b3e5a64d533737f3c2fb77a742bb6
Neichi <hotdlolxp12@gmail.com>
2025-06-07 18:43:13 -0500

    Update Category.jsx

--- ucvfrontend/src/pages/Category.jsx
+++ ucvfrontend/src/pages/Category.jsx
@@ -1,14 +1,59 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+import {
+  FaTachometerAlt,
+  FaClipboardList,
+  FaTags,
+  FaFileAlt,
+  FaBuilding,
+  FaUsers,
+  FaUserPlus,
+} from 'react-icons/fa';
 import CategoryForm from '../components/Category/CategoryForm/CategoryForm';
 import CategoryTable from '../components/Category/CategoryTable/CategoryTable';
 
+
 const Category = () => {
   return (
-    <>
-      <CategoryForm/>
-      <CategoryTable/>
-    </>
+    <div className="main-layout">
+      {/* ===== SIDEBAR ===== */}
+      <div className="sidebar">
+        <Link to="/dashboard" className="sidebar-item">
+          <FaTachometerAlt />
+          <span>Dashboard</span>
+        </Link>
+        <Link to="/incidencias" className="sidebar-item">
+          <FaClipboardList />
+          <span>Incidencias</span>
+        </Link>
+        <Link to="/categoria" className="sidebar-item">
+          <FaTags />
+          <span>Categoría</span>
+        </Link>
+        <Link to="/informe" className="sidebar-item">
+          <FaFileAlt />
+          <span>Informe</span>
+        </Link>
+        <Link to="/departamento" className="sidebar-item">
+          <FaBuilding />
+          <span>Departamento</span>
+        </Link>
+        <Link to="/personal" className="sidebar-item">
+          <FaUsers />
+          <span>Personal</span>
+        </Link>
+        <Link to="/asignar-personal" className="sidebar-item">
+          <FaUserPlus />
+          <span>Asignar</span>
+        </Link>
+      </div>
+
+      {/* ===== ÁREA DE CONTENIDO ===== */}
+      <div className="content-area">
+        <CategoryForm />
+        <CategoryTable />
+      </div>
+    </div>
   );
 };
 
