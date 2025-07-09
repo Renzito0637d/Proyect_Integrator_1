a432be9bef417100cd06b9979b73fd482ba67426
renzito0637d <renzito0637d@gmail.com>
2025-07-09 00:38:07 -0500

    Service Report

---
+++ ucvfrontend/src/components/Report/ProcessReport.jsx
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import { getAuthHeader } from '../../Utils/Auth';
+
+export const getAllAssign = async () => {
+    try {
+        const response = await axios.get(`http://localhost:8080/api/ucv/assignStaffList`, {
+            headers: {
+                'Content-Type': 'application/json',
+                ...getAuthHeader()
+            }
+        })
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const saveReport = async (data) => {
+    try {
+        const respone = await axios.post(`http://localhost:8080/api/ucv/saveReport`,data, {
+            headers: {
+                'Content-Type': 'application/json',
+                ...getAuthHeader()
+            }
+        })
+        return respone.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const getReportId = async (id) => {
+    try {
+        const response = await axios.get(`http://localhost:8080/api/ucv/getAllReport`, {
+            headers: {
+                'Content-Type': 'application/json',
+                ...getAuthHeader()
+            }
+        });
+        return response.data.find(report => String(report.id) === String(id));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const updateReport = async (id, data) => {
+    try {
+        const response = await axios.put(`http://localhost:8080/api/ucv/updateReport/${id}`, data, {
+            headers: {
+                'Content-Type': 'application/json',
+                ...getAuthHeader()
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const delateReport = async (id) => {
+    try {
+        const response = await axios.delete(`http://localhost:8080/api/ucv/deleteReport/${id}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                ...getAuthHeader()
+            },
+        })
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
