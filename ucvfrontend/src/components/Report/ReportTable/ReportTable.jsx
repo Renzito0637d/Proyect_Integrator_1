6b5d4367075d27272190bc8026a361b5146f4883
renzito0637d <renzito0637d@gmail.com>
2025-07-10 04:26:09 -0500

    Update component ReportTable

--- ucvfrontend/src/components/Report/ReportTable/ReportTable.jsx
+++ ucvfrontend/src/components/Report/ReportTable/ReportTable.jsx
@@ -35,54 +35,54 @@ function ReportTable({ reports = [] }) {
       <div className="d-flex flex-wrap bg-light p-3 rounded border col-12 gap-2">
         {/* Tabla principal */}
         <div className="flex-grow-1 col-12 col-md-10">
-          <div className="scroll">
-            <table className="table table-bordered table-sm text-center align-middle">
-              <thead className="table-light">
-                <tr>
-                  <th>ID</th>
-                  <th>Acciones Tomadas</th>
-                  <th>Id asignacion</th>
-                  <th>Estados</th>
-                  <th>Personal</th>
-                  <th>Descripción</th>
-                  <th>Fecha</th>
-                </tr>
-              </thead>
-              <tbody>
-                {reports.length === 0 ? (
-                  <tr>
-                    <td colSpan="7" className="text-center">Sin datos</td>
-                  </tr>
-                ) : (
-                  reports.map((report) => {
-                    return (
-                      <tr key={report.id}>
-                        <td>{report.id}</td>
-                        <td>{report.actions}</td>
-                        <td>{report.assignStaff ? report.assignStaff.id : '-'}</td> {/* Verifica si assignStaff existe */}
-                        <td>{report.status}</td>
-                        <td>{report.user ? report.user.nickname : '-'}</td> {/* Verifica si user existe */}
-                        <td>{report.descripcion}</td>
-                        <td>
-                          {report.resolutionDate
-                            ? new Date(report.resolutionDate).toLocaleString('es-PE', {
-                              day: '2-digit',
-                              month: '2-digit',
-                              year: 'numeric',
-                              hour: '2-digit',
-                              minute: '2-digit',
-                              hour12: true,
-                            })
-                            : "-"}
-                        </td>
-                      </tr>
-                    );
-                  })
-                )}
 
-              </tbody>
-            </table>
-          </div>
+          <table className="table table-bordered table-sm text-center align-middle">
+            <thead className="table-light">
+              <tr>
+                <th>ID</th>
+                <th>Acciones Tomadas</th>
+                <th>Id asignacion</th>
+                <th>Estados</th>
+                <th>Personal</th>
+                <th>Descripción</th>
+                <th>Fecha</th>
+              </tr>
+            </thead>
+            <tbody>
+              {reports.length === 0 ? (
+                <tr>
+                  <td colSpan="7" className="text-center">Sin datos</td>
+                </tr>
+              ) : (
+                reports.map((report) => {
+                  return (
+                    <tr key={report.id}>
+                      <td>{report.id}</td>
+                      <td>{report.actions}</td>
+                      <td>{report.assignStaff ? report.assignStaff.id : '-'}</td> {/* Verifica si assignStaff existe */}
+                      <td>{report.status}</td>
+                      <td>{report.user ? report.user.nickname : '-'}</td> {/* Verifica si user existe */}
+                      <td>{report.descripcion}</td>
+                      <td>
+                        {report.resolutionDate
+                          ? new Date(report.resolutionDate).toLocaleString('es-PE', {
+                            day: '2-digit',
+                            month: '2-digit',
+                            year: 'numeric',
+                            hour: '2-digit',
+                            minute: '2-digit',
+                            hour12: true,
+                          })
+                          : "-"}
+                      </td>
+                    </tr>
+                  );
+                })
+              )}
+
+            </tbody>
+          </table>
+
         </div>
 
         {/* Panel lateral derecho */}
